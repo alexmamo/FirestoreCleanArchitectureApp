70f1fd7ca5d2a40960823fd4ceff9c3821b53c93
alexm <alexmamo@gmail.com>
2021-10-05 16:30:56 +0300

    Minor fixes and improvements.

--- app/src/main/java/ro/alexmamo/firestorecleanarchitecture/presentation/books/BooksActivity.kt
+++ app/src/main/java/ro/alexmamo/firestorecleanarchitecture/presentation/books/BooksActivity.kt
@@ -1,4 +1,4 @@
-package ro.alexmamo.firestorecleanarchitecture.presentation
+package ro.alexmamo.firestorecleanarchitecture.presentation.books
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
