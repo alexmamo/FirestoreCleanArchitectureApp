52e6e35e2cd03f6a1e221d76b8cff870770b7492
Alex Mamo <alexmamo@gmail.com>
2023-03-25 15:18:47 +0200

    Minor fixes and improvements.

--- app/src/main/java/ro/alexmamo/firestorecleanarchitecture/domain/model/Book.kt
+++ app/src/main/java/ro/alexmamo/firestorecleanarchitecture/domain/model/Book.kt
@@ -1,6 +1,6 @@
 package ro.alexmamo.firestorecleanarchitecture.domain.model
 
-data class Book (
+data class Book(
     var id: String? = null,
     var title: String? = null,
     var author: String? = null
